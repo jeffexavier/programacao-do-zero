586b6f019650a6bb530ff9e342a1f394348c367d
Ana Alura <jeffexavier@gmail.com>
2022-10-28 00:10:30 -0300

    Desafio for

---
+++ estruturas de controle/for/desafio_for.js
@@ -0,0 +1,21 @@
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+
+/*
+let linha = ''
+
+for(let i = 1 ; i <= 6; i++) {
+    linha += "#"
+    console.log(linha);
+}
+*/
+
+for(let linha = '#'; linha !== '#######'; linha += '#') {
+    console.log(linha)
+}
+
+//array -> foreach
