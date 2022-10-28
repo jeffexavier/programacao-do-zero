640396c2cbb7e41e86a3e0d95d34d425bdbf7745
Ana Alura <jeffexavier@gmail.com>
2022-10-28 01:16:48 -0300

    Início dos exercícios switch

---
+++ estruturas de controle/switch/switch_1.js
@@ -0,0 +1,20 @@
+let nota = 12;
+
+switch(Math.ceil(nota)) {
+    case 10: case 9: case 8: case 7:
+        console.log('Parabéns!');
+        break;
+    case 6: case 5:    
+        console.log('Recuperação!');
+        break;
+    case 4: case 3: case 2:
+        console.log('Reprovado!');
+        break;
+    case 1: case 0:
+        console.log('Reprovado com louvor!');
+        break;
+    default:
+        console.log('Nota inválida!');
+};
+
+console.log('Fim!')
