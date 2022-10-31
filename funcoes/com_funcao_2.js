b7686328e4c8e8ca20ea21623a4594b51353ad9d
Ana Alura <jeffexavier@gmail.com>
2022-10-30 21:01:25 -0300

    Pasta  funcao

---
+++ funcoes/com_funcao_2.js
@@ -0,0 +1,21 @@
+
+let d1 = 28
+let m1 = 05
+let a1 = 1997
+
+let d2 = 22
+let m2 = 01
+let a2 = 1996
+
+
+function exibirData(d, m, a) {
+    console.log(`${d}/${m}/${a}`);
+}
+
+exibirData(d1, m1, a1);
+exibirData(d1, m1, a1);
+exibirData(d1, m1, a1);
+
+exibirData(d2, m2, a2);
+exibirData(d2, m2, a2);
+exibirData(d2, m2, a2);
