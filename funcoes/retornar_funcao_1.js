ad8bf194451ba657d5cec139946f83e78d987c0c
Ana Alura <jeffexavier@gmail.com>
2022-11-06 18:29:29 -0300

    estudo sobre funcoes dentro de funcoes

---
+++ funcoes/retornar_funcao_1.js
@@ -0,0 +1,16 @@
+function retornaUmaFuncao() {
+    
+    function bomDia() {
+        return "Bom dia!!!";
+    }
+    
+    return bomDia;
+}
+
+
+console.log(retornaUmaFuncao);
+console.log(retornaUmaFuncao());
+console.log(retornaUmaFuncao()());
+
+const umaFuncao = retornaUmaFuncao();
+console.log(umaFuncao());
